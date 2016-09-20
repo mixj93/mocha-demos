52f418e8b38b74a2c69dd944e48a7b179d736918
Li Xueli <mixj93@163.com>
2016-09-20 10:33:51 +0800

    add test

--- demo01/add.test.js
+++ demo01/add.test.js
@@ -5,4 +5,7 @@ describe('加法函数的测试', function() {
   it('1 加 1 应该等于 2', function() {
     expect(add(1, 1)).to.be.equal(2);
   });
+  it('1 加 0 应该等于 1', function() {
+    expect(add(1, 0)).to.be.equal(1);
+  });
 });
